ba588175003a26a1f4dd57462b2242964500250d
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-06-03 15:20:16 +0100

    feat(App.tsx): add routes for Home, Article, Login, and UserProfile pages to the Routes component to enable navigation within the app feat(NavTop.tsx): create NavTop component to be used for the top navigation bar feat(NavTopView.css): create CSS file for styling the NavTop component feat(NavTopView.tsx): create NavTopView component to render the view of the NavTop component feat(config.json): add configuration file with routes defined for the app feat(Article.tsx): create Article page component feat(Home.tsx): create Home page component feat(Login.tsx): create Login page component feat(UserProfile.tsx): create UserProfile page component
    The changes were made to add the necessary components and routes for the Home, Article, Login, and UserProfile pages in the app. The NavTop component and its related files were also added to create a navigation bar for the app. The config.json file was added to store the routes configuration for easy access and maintenance.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { Routes, Route } from "react-router-dom";
 
+import Article from "./pages/Article";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+
 import { useAuth } from "./hooks/useAuth";
 import { AuthContext } from "./context/auth-context";
+import config from "./config.json";
+
 import "./App.css";
 
 function App() {
+  const { routes } = config;
   const { isLoggedIn, user, login, logout } = useAuth();
+
   return (
     <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>
-      <Routes></Routes>
+      <Routes>
+        <Route path={routes.home.path} element={<Home />} />
+        <Route path={routes.article.path} element={<Article />} />
+        <Route path={routes.userProfile.path} element={<UserProfile />} />
+        <Route path={routes.login.path} element={<Login />} />
+      </Routes>
     </AuthContext.Provider>
   );
 }
