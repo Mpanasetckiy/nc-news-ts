1ef6960197301719d96cd51b1349e387189aa1f8
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-06-03 19:19:18 +0100

    feat(App.tsx): add wildcard route to render Home component for all unmatched routes feat(NavBottom.tsx): create NavBottom component to render a bottom navigation bar feat(NavBottomView.css): add styles for NavBottomView component feat(NavBottomView.tsx): create NavBottomView component to render the content of NavBottom component feat(NavLink.tsx): create NavLink component to render a navigation link with active/inactive styling feat(config.json): add createArticle route to config feat(Home.tsx): add NavBottom component to Home page feat(UserProfile.tsx): add NavBottom component to UserProfile page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import NavTop from "../components/NavTop/NavTop";
+import NavBottom from "../components/NavBottom/NavBottom";
 
 const Home = () => {
   return (
     <>
       <NavTop isPage={true} title="Articles" />
+      <NavBottom />
     </>
   );
 };
