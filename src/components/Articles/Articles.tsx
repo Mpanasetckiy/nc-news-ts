55c17c99b7ee93f485a4fd0bf49bf9e1a278c515
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-08-24 17:47:07 +0100

    fix(Articles.tsx): replace empty fragment with Skeleton component from antd library to display a loading skeleton when articles are being fetched

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Skeleton } from "antd";
 
 import { useAxios } from "../../hooks/useAxios";
 
@@ -32,8 +33,8 @@ const Articles = () => {
     }
   };
 
-  if (!articles.length) {
-    return <></>;
+  if (isLoading) {
+    return <Skeleton active avatar></Skeleton>;
   }
 
   return (
